18b9fb6b0b9781177d1e12a960e9883c277d9bc0
paddybeefcakes <fpickering@humanvector.com>
2018-10-21 21:44:32 -0400

    Init commit

---
+++ ChartStyle.R
@@ -0,0 +1,16 @@
+caTheme <- hc_theme(
+  colors = c("#0f5785", "#ff7e30", "#A2DCE6", "#caed2e", "#0c3254"),
+  chart = list(
+    backgroundColor = "#f2eee6"
+  ),
+  title = list(
+    style = list(
+      color = '#0f5785'
+    )
+  ),
+  subtitle = list(
+    style = list(
+      color = '#0f5785'
+    )
+  )
+)
